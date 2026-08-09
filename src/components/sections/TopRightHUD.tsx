e36236cecef842d1d567762691921cb31b6bb2e1
FNYukii <forestgreen14689@gmail.com>
2026-08-10 00:37:36 +0900

    feat: 天気セクション

--- src/components/sections/TopRightHUD.tsx
+++ src/components/sections/TopRightHUD.tsx
@@ -2,6 +2,18 @@ import clsx from 'clsx'
 import dayjs from 'dayjs'
 import { useState, useEffect } from 'react'
 import { TypingText } from '../parts/TypingText'
+import { getRandomItem, getRandomNum } from '../../utils/random'
+
+const WEATHERS = [
+  'clear',
+  'sunny',
+  'cloudy',
+  'rainy',
+  'snowy',
+  'windy',
+  'foggy',
+  'thunderstorm',
+]
 
 const ClockSection = () => {
   const [now, setNow] = useState(dayjs())
@@ -15,17 +27,34 @@ const ClockSection = () => {
   }, [])
 
   return (
-    <section className={clsx('flex flex-col items-end', '*:text-2xl')}>
+    <section className={clsx('flex flex-col items-end', '*:text-3xl')}>
       <TypingText text={now.format('YYYY-MM-DD dddd')} />
       <TypingText text={now.format('HH:mm:ss')} startDelay={400} />
     </section>
   )
 }
 
+const WeatherSection = () => {
+  const weather = getRandomItem(WEATHERS)
+  const temperature = getRandomNum(10, 40)
+  const humidity = getRandomNum(30, 70)
+
+  return (
+    <section>
+      <TypingText
+        text={`${weather} | ${temperature}℃ | ${humidity}%`}
+        startDelay={600}
+        className="mt-1 text-xl"
+      />
+    </section>
+  )
+}
+
 export const TopRightHUD = () => {
   return (
-    <div>
+    <div className="flex flex-col items-end">
       <ClockSection />
+      <WeatherSection />
     </div>
   )
 }
---
+++ src/utils/random.ts
@@ -0,0 +1,18 @@
+/**
+ * 受け取った配列からランダムな要素を返却
+ */
+export const getRandomItem = <T>(items: T[]): T => {
+  const randomIndex = Math.floor(Math.random() * items.length)
+  return items[randomIndex]
+}
+
+/**
+ * 指定した範囲内でランダムな数値を返す
+ *
+ * - 形式は小数点1位まで
+ * - 20, 30と指定すれば 20.0 ~ 29.9 を返す
+ */
+export const getRandomNum = (min: number, max: number) => {
+  const randomNum = Math.random() * (max - min) + min
+  return Number(randomNum.toFixed(1))
+}

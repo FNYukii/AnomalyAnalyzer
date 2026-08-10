e5354128a696c1835ec10d98d041b118f970b4ab
FNYukii <forestgreen14689@gmail.com>
2026-08-11 01:07:14 +0900

    feat: 配色調整

--- src/features/BottomHUD/views/BottomHUD.tsx
+++ src/features/BottomHUD/views/BottomHUD.tsx
@@ -5,13 +5,13 @@ export const BottomHUD = () => {
   return (
     <div className={clsx('h-full w-fit', 'grid grid-rows-2 gap-4')}>
       <section
-        className={clsx('aspect-video', 'border border-primary bg-primary/10')}
+        className={clsx('aspect-video', 'border border-primary bg-primary/15')}
       >
         <BarWaveformChart />
       </section>
 
       <section
-        className={clsx('aspect-video', 'border border-primary bg-primary/10')}
+        className={clsx('aspect-video', 'border border-primary bg-primary/15')}
       >
         <BarWaveformChart />
       </section>

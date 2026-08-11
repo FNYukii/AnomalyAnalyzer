6c4f0ac130b797338903e2abf849ba4c418e3d1d
FNYukii <forestgreen14689@gmail.com>
2026-08-11 14:48:30 +0900

    feat: 波形グラフをフェードイン

---
+++ src/features/BottomHUD/elements/FadeIn.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState, type ReactElement } from 'react'
+import clsx from 'clsx'
+
+type Props = {
+  // NOTE: ReactElement で単一のReact要素と保証
+  children: ReactElement<{ className?: string }>
+}
+
+export const FadeIn = ({ children }: Props) => {
+  const [isVisible, setIsVisible] = useState(false)
+
+  useEffect(() => {
+    setIsVisible(true)
+  }, [])
+
+  return React.cloneElement(children, {
+    className: clsx(
+      children.props.className,
+      'transition duration-300',
+      isVisible ? 'opacity-100' : 'opacity-0',
+    ),
+  })
+}
--- src/features/BottomHUD/views/BottomHUD.tsx
+++ src/features/BottomHUD/views/BottomHUD.tsx
@@ -1,30 +1,35 @@
 import clsx from 'clsx'
 import { BarWaveformChart } from '../parts/BarWaveformChart'
 import { DisplayDelay } from '../../misc/elements/DisplayDelay'
+import { FadeIn } from '../elements/FadeIn'
 
 export const BottomHUD = () => {
   return (
     <div className={clsx('h-full w-fit', 'grid grid-rows-2 gap-4')}>
       <DisplayDelay delay={3000}>
-        <section
-          className={clsx(
-            'aspect-video',
-            'border border-primary bg-primary/15',
-          )}
-        >
-          <BarWaveformChart />
-        </section>
+        <FadeIn>
+          <section
+            className={clsx(
+              'aspect-video',
+              'border border-primary bg-primary/15',
+            )}
+          >
+            <BarWaveformChart />
+          </section>
+        </FadeIn>
       </DisplayDelay>
 
       <DisplayDelay delay={3400}>
-        <section
-          className={clsx(
-            'aspect-video',
-            'border border-primary bg-primary/15',
-          )}
-        >
-          <BarWaveformChart />
-        </section>
+        <FadeIn>
+          <section
+            className={clsx(
+              'aspect-video',
+              'border border-primary bg-primary/15',
+            )}
+          >
+            <BarWaveformChart />
+          </section>
+        </FadeIn>
       </DisplayDelay>
     </div>
   )

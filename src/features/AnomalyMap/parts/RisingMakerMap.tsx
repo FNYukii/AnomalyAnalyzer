237d9b0ff3d46b6120791698a1b16205dbb9ac41
FNYukii <forestgreen14689@gmail.com>
2026-08-12 16:36:38 +0900

    clean: Mapを2分割

---
+++ src/features/AnomalyMap/parts/RisingMakerMap.tsx
@@ -0,0 +1,13 @@
+import clsx from 'clsx'
+
+type Props = {
+  className?: string
+}
+
+export const RisingMakerMap = ({ className }: Props) => {
+  return (
+    <div className={clsx('flex justify-center items-end', className)}>
+      {/* <div className="size-4 rounded-full bg-accent animate-rise" /> */}
+    </div>
+  )
+}
--- src/features/AnomalyMap/views/AnomalyMap.tsx
+++ src/features/AnomalyMap/views/AnomalyMap.tsx
@@ -1,13 +1,14 @@
 import { ScrollingSatelliteMap } from '../parts/ScrollingSatelliteMap'
+import { RisingMakerMap } from '../parts/RisingMakerMap'
 
 type Props = {
   className?: string
 }
-
 export const AnomalyMap = ({ className }: Props) => {
   return (
     <div className={className}>
       <ScrollingSatelliteMap />
+      <RisingMakerMap className="fixed inset-0" />
     </div>
   )
 }

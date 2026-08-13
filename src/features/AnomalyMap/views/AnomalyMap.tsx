7eaa75da4c5d0d63078d0a07e96dd05395bc77ee
FNYukii <forestgreen14689@gmail.com>
2026-08-14 00:00:06 +0900

    clean: 空行調整

--- src/features/AnomalyMap/views/AnomalyMap.tsx
+++ src/features/AnomalyMap/views/AnomalyMap.tsx
@@ -5,6 +5,7 @@ import { ScrollingSatelliteMap } from '../parts/ScrollingSatelliteMap'
 type Props = {
   className?: string
 }
+
 export const AnomalyMap = ({ className }: Props) => {
   return (
     <div className={className}>

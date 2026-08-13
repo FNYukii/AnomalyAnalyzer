45fdfeacb9a8688a2b93538704e290304af88542
FNYukii <forestgreen14689@gmail.com>
2026-08-13 15:38:39 +0900

    feat: MapHiderで不要divが残らないように

--- src/features/AnomalyMap/parts/MapHider.tsx
+++ src/features/AnomalyMap/parts/MapHider.tsx
@@ -6,33 +6,24 @@ const ROW_COUNT = 6
 
 const INTERVAL_DELAY = 10
 
-const makeHideFlags = (flagCount: number) => {
-  return [...Array(flagCount)].map(() => true)
+const makeNumbers = (count: number): number[] => {
+  return [...Array(count)].map((i) => i)
 }
 
-type MapHiderProps = {
+type Props = {
   className?: string
 }
 
 /**
  * 画面上を隠すセルが1つずつ消えていくグリッド
  */
-export const MapHider = ({ className }: MapHiderProps) => {
-  const [hideFlags, setHideFlags] = useState(
-    makeHideFlags(COL_COUNT * ROW_COUNT),
-  )
+export const MapHider = ({ className }: Props) => {
+  const [numbers, setNumbers] = useState(makeNumbers(COL_COUNT * ROW_COUNT))
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      setHideFlags((prev) => {
-        const firstTrueIndex = prev.findIndex((item) => item === true)
-        if (firstTrueIndex === -1) {
-          clearInterval(intervalId)
-          return prev
-        }
-        const newHideFlags = [...prev]
-        newHideFlags[firstTrueIndex] = false
-        return newHideFlags
+      setNumbers((prev) => {
+        return prev.slice(0, -1)
       })
     }, INTERVAL_DELAY)
 
@@ -43,14 +34,14 @@ export const MapHider = ({ className }: MapHiderProps) => {
 
   return (
     <div
-      className={clsx(`grid`, className)}
+      className={clsx(`grid`, 'rotate-180', className)}
       style={{
         gridTemplateColumns: `repeat(${COL_COUNT}, minmax(0, 1fr))`,
         gridTemplateRows: `repeat(${ROW_COUNT}, minmax(0, 1fr))`,
       }}
     >
-      {hideFlags.map((isHide) => (
-        <div className={clsx(isHide && 'bg-black')}></div>
+      {numbers.map(() => (
+        <div className="bg-black" />
       ))}
     </div>
   )
